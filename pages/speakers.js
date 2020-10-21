2a3e3d3d21d0b9ee9f1f2d94dda18807ce98a10d
Laura del Toro <lauradeltoro.arq@gmail.com>
2020-10-21 19:04:35 +0100

    Add render props and Context options

--- pages/speakers.js
+++ pages/speakers.js
@@ -1,8 +1,17 @@
+import React from 'react';
 import Header from '../src/components/Header/Header';
 import Menu from '../src/components/Menu/Menu';
 import Speakers from '../src/components/Speakers/Speakers';
 import SpeakersSearchBar from '../src/components/SpeakersSearchBar/SpeakersSearchBar';
 import Footer from '../src/components/Footer/Footer';
+
+import SpeakerContext from '../src/components/Speakers/SpeakerContext';
+
+const speakers = [
+    { imageSrc: 'speaker-component-1124', name: 'Douglas Crockford' },
+    { imageSrc: 'speaker-component-1530', name: 'Tamara Baker' },
+    { imageSrc: 'speaker-component-10803', name: 'Eugene Chuvyrov' },
+]
 
 const SpeakersPage = () => {
     return (
@@ -10,8 +19,10 @@ const SpeakersPage = () => {
             <h1>Speakers page</h1>
             <Header />
             <Menu />
-            <SpeakersSearchBar />
-            <Speakers />
+            <SpeakerContext.Provider value={speakers}>
+                <SpeakersSearchBar />
+                <Speakers />
+            </SpeakerContext.Provider>
             <Footer />
         </div>
     );
--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -1,14 +1,40 @@
-import React from 'react';
-import { withData } from './withData'
+import React, { useContext } from 'react';
+import SpeakerRenderProps from './SpeakersRenderProps';
+import SpeakerContext from './SpeakerContext';
 
-const Speakers = ({ speakers }) => {
-
-
+// ----- Option 1: Context -----
+const Speakers = () => {
+    const speakers = useContext(SpeakerContext);
     return (
         <div>
             {speakers.map(speaker => <p>{speaker.name}</p>)}
         </div>
-    );
+    )
 }
 
-export default withData(2)(Speakers);
+export default Speakers;
+
+// ----- Option 2: HOC -----
+// import { withData } from './withData'
+
+// const Speakers = ({ speakers }) => {
+
+//     return (
+//         <div>
+//             {speakers.map(speaker => <p>{speaker.name}</p>)}
+//         </div>
+//     );
+// }
+
+// export default withData(2)(Speakers);
+
+// ----- Option 3: Render props -----
+// const Speakers = () => {
+//     return (
+//         <SpeakerRenderProps>
+//             {({ speakers }) => speakers.map(speaker => <p>{speaker.name}</p>)}
+//         </SpeakerRenderProps>
+//     )
+// }
+
+// export default Speakers;
