14e4781541391ff5f1b5c7e7bc0685b69a0cba8d
Laura del Toro <lauradeltoro.arq@gmail.com>
2020-11-18 19:52:34 +0000

    Generate false REST API

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -1,55 +1,23 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import SpeakersSearchBar from '../SpeakersSearchBar/SpeakersSearchBar';
 // import SpeakerRenderProps from './SpeakersRenderProps';
 // import SpeakerContext from './SpeakerContext';
 import Speaker from '../Speaker/Speaker'
+import axios from 'axios';
 
 
 const Speakers = () => {
 
-
-    const speakersArray = [
-        {
-            imageSrc: 'speaker-component-1124',
-            name: 'Douglas Crockford',
-            id: 1124,
-            firstName: 'Douglas',
-            lastName: 'Crockford',
-            sat: true,
-            sun: false,
-            isFavorite: false,
-            bio:
-                'Douglas Crockford discovered the JSON Data Interchange Format. He is also the author of _JavaScript: The Good Parts_. He has been called a guru, but he is actually more of a mahatma.',
-        },
-        {
-            imageSrc: 'speaker-component-1530',
-            name: 'Tamara Baker',
-            id: 1530,
-            firstName: 'Tamara',
-            lastName: 'Baker',
-            sat: false,
-            sun: true,
-            isFavorite: true,
-            bio:
-                'Tammy has held a number of executive and management roles over the past 15 years, including VP engineering Roles at Molekule Inc., Cantaloupe Systems, E-Color, and Untangle Inc.',
-        },
-        {
-            imageSrc: 'speaker-component-10803',
-            name: 'Eugene Chuvyrov',
-            id: 10803,
-            firstName: 'Eugene',
-            lastName: 'Chuvyrov',
-            sat: true,
-            sun: false,
-            isFavorite: false,
-            bio:
-                'Eugene Chuvyrov is  a Senior Cloud Architect at Microsoft. He works directly with both startups and enterprises to enable their solutions in Microsoft cloud, and to make Azure better as a result of this work with partners.',
-        },
-    ];
-
     const [searchQuery, setSearchQuery] = useState("");
-    const [speakers, setSpeakers] = useState(speakersArray);
+    const [speakers, setSpeakers] = useState([]);
 
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get('http://localhost:4000/speakers');
+            setSpeakers(response.data);
+        }
+        fetchData();
+    }, [])
     const toggleSpeakerFavourite = speakerToUpdate => {
         return {
             ...speakerToUpdate,
@@ -57,14 +25,16 @@ const Speakers = () => {
         }
     }
 
-    const onFavouriteToggleHandler = speakerToUpdate => {
+    const onFavouriteToggleHandler = async speakerToUpdate => {
         const speakerWithFavouriteToggled = toggleSpeakerFavourite(speakerToUpdate);
         const speakersStateCopy = [...speakers];
         const speakerIndex = speakers.map(speaker => speaker.id).indexOf(speakerToUpdate.id);
         speakersStateCopy.splice(speakerIndex, 1, speakerWithFavouriteToggled);
 
+        await axios.put(`http://localhost:4000/speakers/${speakerToUpdate.id}`, speakerWithFavouriteToggled);
         setSpeakers(speakersStateCopy);
     }
+
     return (
         <div>
             <SpeakersSearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
